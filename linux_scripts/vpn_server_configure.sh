4eff5c1072928f7e4e89735a422ed0e3dfd5cb46
Matthew David Miller <MatthewMiller@mdmiller.dev>
2020-05-21 22:55:28 -0400

    Fixed firewall rules for vpn.

--- linux_scripts/vpn_server_configure.sh
+++ linux_scripts/vpn_server_configure.sh
@@ -22,6 +22,7 @@ dns_address='1.1.1.1'
 network_prefix='10.0.0.0/8'
 wireguard_interface='wg0'
 wireguard_server_ip_address='10.3.0.1'
+wireguard_server_network_prefix='10.3.0.0/24'
 wireguard_server_vpn_key_name='wireguard_vpn_server'
 wireguard_client_key_name='matthew_wireguard'
 wireguard_server_listen_port='64640'
@@ -46,7 +47,7 @@ generate_ssh_key "${user_name}" "y" "n" "n" "${key_name}"
 configure_vpn_scripts "${dynamic_dns}" "${release_name}"
 setup_basic_wireguard_interface "${wireguard_interface}" "${wireguard_server_ip_address}"
 generate_wireguard_key "${user_name}" "${wireguard_server_vpn_key_name}"
-configure_wireguard_server_base "${wireguard_interface}" "${user_name}" "${wireguard_server_vpn_key_name}" "${wireguard_server_ip_address}" "${wireguard_server_listen_port}" "${interface}"
+configure_wireguard_server_base "${wireguard_interface}" "${user_name}" "${wireguard_server_vpn_key_name}" "${wireguard_server_ip_address}" "${wireguard_server_listen_port}" "${interface}" "${wireguard_server_network_prefix}"
 generate_wireguard_key "${user_name}" "${wireguard_client_key_name}"
 add_wireguard_peer "${wireguard_interface}" "${user_name}" "${wireguard_client_key_name}" "${wireguard_client_ip_address}"
 wireguard_create_client_config "${wireguard_interface}" "${user_name}" "${wireguard_client_key_name}" "${wireguard_server_vpn_key_name}" "${wireguard_client_ip_address}" "${wireguard_dns_server}" "${wireguard_public_dns_ip_address}" "${wireguard_server_listen_port}"
