dcce91797c4cc18f877b7ac2a1795b4c7d0e387b
MatthewDavidMiller <matthewdavidmiller1@gmail.com>
2020-07-07 09:45:49 -0400

    Removed unused parameters and variables.

--- linux_scripts/dns_server_configure.sh
+++ linux_scripts/dns_server_configure.sh
@@ -20,7 +20,6 @@ subnet_mask='255.255.255.0'
 gateway_address='10.1.10.1'
 dns_address='1.1.1.1'
 network_prefix='10.0.0.0/8'
-loopback='127.0.0.1'
 ipv6_link_local_address='fe80::5'
 
 # Call functions
@@ -33,7 +32,7 @@ fix_apt_packages
 install_dns_server_packages
 configure_ssh
 generate_ssh_key "${user_name}" "y" "n" "n" "${key_name}"
-iptables_setup_base "${interface}" "${network_prefix}"
+iptables_setup_base
 iptables_allow_ssh "${network_prefix}" "${interface}"
 iptables_allow_dns "${network_prefix}" "${interface}"
 iptables_allow_http "${network_prefix}" "${interface}"
