dc4113470b96e400c37159ed8d1b5d872c95216a
Matthew David Miller <MatthewMiller@mdmiller.dev>
2020-07-07 09:42:58 -0400

    Added ipv6 support.  Added more firewall rules.  Changed how some lines are added to configuration files.

--- linux_scripts/omada_controller_configure.sh
+++ linux_scripts/omada_controller_configure.sh
@@ -20,19 +20,22 @@ subnet_mask='255.255.255.0'
 gateway_address='10.1.10.1'
 dns_address='1.1.1.1'
 network_prefix='10.0.0.0/8'
+ipv6_link_local_address='fe80::7'
 
 # Call functions
 lock_root
 get_username
 get_interface_name
-configure_network "${ip_address}" "${network_address}" "${subnet_mask}" "${gateway_address}" "${dns_address}" "${interface}"
+configure_network "${ip_address}" "${network_address}" "${subnet_mask}" "${gateway_address}" "${dns_address}" "${interface}" "${ipv6_link_local_address}"
 fix_apt_packages
 install_omada_controller_packages
 configure_ssh
 generate_ssh_key "${user_name}" "y" "n" "n" "${key_name}"
-iptables_setup_base "${interface}" "${network_prefix}"
-iptables_allow_ssh "${network_prefix}" "${ip_address}"
-iptables_allow_omada_controller "${network_prefix}" "${ip_address}"
+iptables_setup_base
+iptables_allow_ssh "${network_prefix}" "${interface}"
+iptables_allow_omada_controller "${network_prefix}" "${interface}"
+iptables_allow_icmp "${network_prefix}" "${interface}"
+iptables_allow_loopback
 iptables_set_defaults
 apt_configure_auto_updates "${release_name}"
 configure_omada_controller
