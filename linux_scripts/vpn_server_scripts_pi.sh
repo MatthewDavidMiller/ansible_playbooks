3c362d34e1360dfb023988d108269b199827cf7d
Matthew David Miller <MatthewMiller@mdmiller.dev>
2020-10-18 17:33:54 -0400

    Added an interactive menu.

--- linux_scripts/vpn_server_scripts_pi.sh
+++ linux_scripts/vpn_server_scripts_pi.sh
@@ -30,7 +30,7 @@ function configure_network() {
 
     # Configure network
     grep -q -E ".*auto ${interface}" '/etc/network/interfaces' && sed -i -E "s,.*auto ${interface}.*,auto ${interface}," '/etc/network/interfaces' || printf '%s\n' "auto ${interface}" >>'/etc/network/interfaces'
-    grep -q -E ".*iface ${interface} inet " '/etc/network/interfaces' && sed -i -E "s,.*iface ${interface} inet .*\naddress\nnetwork\nnetmask\ngateway\ndns-nameservers,iface ${interface} inet static\naddress ${ip_address}\nnetwork ${network_address}\nnetmask ${subnet_mask}\ngateway ${gateway_address}\ndns-nameservers ${dns_address}," '/etc/network/interfaces' || cat <<EOF >>'/etc/network/interfaces'
+    grep -q -E ".*iface ${interface} inet" '/etc/network/interfaces' && sed -i -E "s,.*iface ${interface} inet.*\naddress\nnetwork\nnetmask\ngateway\ndns-nameservers,iface ${interface} inet static\naddress ${ip_address}\nnetwork ${network_address}\nnetmask ${subnet_mask}\ngateway ${gateway_address}\ndns-nameservers ${dns_address}," '/etc/network/interfaces' || cat <<EOF >>'/etc/network/interfaces'
 iface ${interface} inet static
     address ${ip_address}
     network ${network_address}
