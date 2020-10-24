a192550574699490ef8ebbc8920668a5c70deced
Matthew David Miller <MatthewMiller@mdmiller.dev>
2020-10-23 22:57:35 -0400

    Updated table ids.

--- linux_scripts/dns_server_scripts.sh
+++ linux_scripts/dns_server_scripts.sh
@@ -238,37 +238,37 @@ function configure_pihole() {
     # Configure allowlist, denylist, and regex
     # Possible values: id, type, domain, enabled, date_added, date_modified, comment
     sqlite3 /etc/pihole/gravity.db <<EOF
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (1,3,'^.+\.(ru|cn|ro|ml|ga|gq|cf|tk|pw|ua|ug|ve|)$',1,'Block some country TLDs.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (2,3,'porn',1,'Block domains with the word porn in them.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (3,3,'sex',1,'Block domains with the word sex in them.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (4,0,'ntscorp.ru',1,'Openiv mod download domain.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (5,3,'date',1,'Block domains with the word date in them.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (6,3,'love',1,'Block domains with the word love in them.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (7,2,'update',1,'Allow domains with the word update in them.');
-INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (8,3,'hentai',1,'Block domains with the word hentai in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (10,3,'^.+\.(ru|cn|ro|ml|ga|gq|cf|tk|pw|ua|ug|ve|)$',1,'Block some country TLDs.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (11,3,'porn',1,'Block domains with the word porn in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (12,3,'sex',1,'Block domains with the word sex in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (13,0,'ntscorp.ru',1,'Openiv mod download domain.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (14,3,'date',1,'Block domains with the word date in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (15,3,'love',1,'Block domains with the word love in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (16,2,'update',1,'Allow domains with the word update in them.');
+INSERT INTO domainlist (id, type, domain, enabled, comment) VALUES (17,3,'hentai',1,'Block domains with the word hentai in them.');
 EOF
 
     # Configure blocklists
     # Possible values: id, address, enabled, date_added, date_modified, comment
     sqlite3 /etc/pihole/gravity.db <<EOF
-INSERT INTO adlist (id, address, enabled, comment) VALUES (1,'https://mirror1.malwaredomains.com/files/justdomains',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (2,'https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_all.list',1,'A porn blocklist.');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (3,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',0,'Default All in one blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (4,'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt',0,'Default Tracker blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (5,'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt',0,'Default Ad blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (6,'https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (7,'https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (8,'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (9,'https://v.firebog.net/hosts/Prigent-Crypto.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (10,'https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (11,'https://www.malwaredomainlist.com/hostslist/hosts.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (12,'https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (13,'https://phishing.army/download/phishing_army_blocklist_extended.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (14,'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (15,'https://v.firebog.net/hosts/Shalla-mal.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (16,'https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (17,'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts',1,'malware blocklist');
-INSERT INTO adlist (id, address, enabled, comment) VALUES (18,'https://urlhaus.abuse.ch/downloads/hostfile/',0,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (10,'https://mirror1.malwaredomains.com/files/justdomains',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES 11,'https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_all.list',1,'A porn blocklist.');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (12,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',0,'Default All in one blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (13,'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt',0,'Default Tracker blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (14,'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt',0,'Default Ad blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (15,'https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (16,'https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (17,'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (18,'https://v.firebog.net/hosts/Prigent-Crypto.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (19,'https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (20,'https://www.malwaredomainlist.com/hostslist/hosts.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (21,'https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (22,'https://phishing.army/download/phishing_army_blocklist_extended.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (23,'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (24,'https://v.firebog.net/hosts/Shalla-mal.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (25,'https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (26,'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts',1,'malware blocklist');
+INSERT INTO adlist (id, address, enabled, comment) VALUES (27,'https://urlhaus.abuse.ch/downloads/hostfile/',0,'malware blocklist');
 
 EOF
 
