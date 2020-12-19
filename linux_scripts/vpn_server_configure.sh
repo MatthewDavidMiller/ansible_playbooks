6c4b1ad4521cc3f36d234347c1f5aed2556bcdd9
Matthew David Miller <MatthewMiller@mdmiller.dev>
2020-12-18 21:10:27 -0500

    Updated function to call.

--- linux_scripts/vpn_server_configure.sh
+++ linux_scripts/vpn_server_configure.sh
@@ -109,7 +109,7 @@ select options_select in "${options[@]}"; do
                 create_user "${user_name}"
                 ;;
             "Add user to sudo")
-                add_user_to_sudo "${user_name}"
+                add_user_to_sudo_group "${user_name}"
                 ;;
             "Create a swap file")
                 create_swap_file "${swap_file_size}"
