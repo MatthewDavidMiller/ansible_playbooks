7f4381eb574ab0078c23f9fd9c346853709e597f
Matthew David Miller <MatthewMiller@mdmiller.dev>
2021-02-27 00:20:04 -0500

    Updated dns_server_scipts.py

--- linux_scripts/dns_server_scripts.py
+++ linux_scripts/dns_server_scripts.py
@@ -1,10 +1,18 @@
 # Credits
 # https://www.tutorialspoint.com/python/os_chmod.htm
+# https://stackabuse.com/how-to-copy-a-file-in-python/
+# https://pynative.com/python-sqlite-insert-into-table/
+# https://stackoverflow.com/questions/38780764/how-to-create-a-list-from-a-text-file-in-python
+# https://www.geeksforgeeks.org/iterate-over-a-list-in-python/
+
 
 import os
 import stat
 import urllib.request
 import subprocess
+import shutil
+import sqlite3
+import re
 
 
 def configure_dns_server_scripts(user_name):
@@ -28,3 +36,46 @@ def configure_dns_server_scripts(user_name):
         opened_file.write(cron_jobs_text + '\n')
     subprocess.call([r'crontab', r'jobs.cron'])
     os.remove(r'jobs.cron')
+
+
+def configure_unbound():
+    urllib.request.urlretrieve(
+        r'https://www.internic.net/domain/named.root', r'/var/lib/unbound/root.hints')
+    subprocess.call([r'systemctl', r'enable', r'unbound'])
+    subprocess.call([r'systemctl', r'start', r'unbound'])
+    shutil.copyfile(r'dns_server_configuration/env/pi-hole.conf',
+                    r'/etc/unbound/unbound.conf.d/pi-hole.conf')
+
+
+def install_pihole(user_name):
+    subprocess.call([r'sudo', r'-u', user_name, r'git', r'clone', r'--depth', r'1',
+                     r'https://github.com/pi-hole/pi-hole.git', r'/home/' + user_name + r'/Pi-hole'])
+    subprocess.call([r'sudo', r'-u', user_name, r'bash', r'/home/' +
+                     user_name + r'/Pi-hole/automated install/basic-install.sh'])
+
+
+def configure_pihole():
+    allow_block_list = open(
+        r'dns_server_configuration/env/allow_block_list.txt').readlines()
+    lists = open(
+        r'dns_server_configuration/env/lists.txt').readlines()
+    shutil.copyfile(r'dns_server_configuration/domains.txt',
+                    r'/etc/pihole/custom.list')
+
+    for i in allow_block_list:
+        sqlite3.connect(r'/etc/pihole/gravity.db').cursor().execute(i)
+
+    for i in lists:
+        sqlite3.connect(r'/etc/pihole/gravity.db').cursor().execute(i)
+
+    shutil.copyfile(r'dns_server_configuration/env/setupVars.conf',
+                    r'/etc/pihole/setupVars.conf')
+
+    print(r'Set Pihole Password')
+    subprocess.call([r'pihole', r'-a', r'-p'])
+
+    uid = pwd.getpwnam(r'pihole').pw_uid
+    gid = pwd.getpwnam(r'pihole').pw_gid
+    os.chown(r'/etc/pihole' + uid, gid)
+    os.chmod(r'/etc/pihole',
+             stat.S_IRWXU + stat.S_IRWXG)
