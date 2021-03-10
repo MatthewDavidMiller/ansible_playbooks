38ba41e62dfc9593639f8e7e8da401adf8b4c88c
Matthew David Miller <MatthewMiller@mdmiller.dev>
2021-03-09 21:02:01 -0500

    Added new functions to vpn_server_scripts.py

--- linux_scripts/vpn_server_scripts.py
+++ linux_scripts/vpn_server_scripts.py
@@ -40,3 +40,54 @@ def generate_wireguard_key(user_name, key_name):
     os.chown(r'/home/' + user_name + r'/.wireguard_keys' + uid, gid)
 
     os.chmod(r'/home/' + user_name + r'/.wireguard_keys', stat.S_IRWXU)
+
+
+def configure_wireguard_server_base(wireguard_interface, user_name, server_key_name, ip_address, listen_port, network_interface, vpn_network_prefix):
+    with open(r'/home/' + user_name + r'/.wireguard_keys/' + server_key_name, "r") as opened_file:
+        private_key = opened_file.read()
+
+    config_text = r'[Interface]' + '\n' + r'Address = ' + ip_address + r'/24' + '\n' + r'ListenPort = ' + listen_port + '\n' + r'PrivateKey = ' + private_key + '\n' + r'PostUp = iptables -A FORWARD -d ' + vpn_network_prefix + r' -i ' + network_interface + r' -o ' + wireguard_interface + r' -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT; iptables -A FORWARD -s ' + vpn_network_prefix + r' -i ' + wireguard_interface + r' -o ' + network_interface + r' -j ACCEPT; iptables -t nat -I POSTROUTING -s ' + \
+        vpn_network_prefix + r' -o ' + network_interface + r' -j MASQUERADE' + '\n' + r'PostDown = iptables -A FORWARD -d ' + vpn_network_prefix + r' -i ' + network_interface + r' -o ' + wireguard_interface + r' -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT; iptables -A FORWARD -s ' + \
+        vpn_network_prefix + r' -i ' + wireguard_interface + r' -o ' + network_interface + \
+        r' -j ACCEPT; iptables -t nat -I POSTROUTING -s ' + \
+        vpn_network_prefix + r' -o ' + network_interface + r' -j MASQUERADE'
+
+    with open(r'/etc/wireguard/' + wireguard_interface + r'.conf', "w") as opened_file:
+        opened_file.write(config_text + '\n')
+
+
+def add_wireguard_peer(interface, user_name, client_key_name, ip_address):
+    with open(r'/home/' + user_name + r'/.wireguard_keys/' + client_key_name + r'.pub', "r") as opened_file:
+        public_key = opened_file.read()
+
+    config_text = r'# ' + client_key_name + '\n' + \
+        r'[Peer]' + '\n' + r'PublicKey = ' + public_key + \
+        '\n' + r'AllowedIPs = ' + ip_address + r'/32'
+
+    with open(r'/etc/wireguard/' + interface + r'.conf', "w") as opened_file:
+        opened_file.write(config_text + '\n')
+
+
+def wireguard_create_client_config(user_name, client_key_name, server_key_name, ip_address, dns_server, public_dns_ip_address, listen_port):
+    with open(r'/home/' + user_name + r'/.wireguard_keys/' + client_key_name, "r") as opened_file:
+        private_key = opened_file.read()
+
+    with open(r'/home/' + user_name + r'/.wireguard_keys/' + client_key_name + r'.pub', "r") as opened_file:
+        public_key = opened_file.read()
+
+    os.makedirs(r'/home/' + user_name +
+                r'/.wireguard_client_configs', exist_ok=True)
+
+    config_text = r'[Interface]' + '\n' + r'Address = ' + ip_address + '\n' + r'PrivateKey = ' + private_key + '\n' + r'DNS = ' + dns_server + '\n\n' + \
+        r'[Peer]' + '\n' + r'PublicKey = ' + public_key + '\n' + r'AllowedIPs = 0.0.0.0/0, ::/0' + \
+        '\n' + r'Endpoint = ' + public_dns_ip_address + r':' + listen_port
+
+    with open(r'/home/' + user_name + r'/.wireguard_client_configs/' + client_key_name + r'.conf', "w") as opened_file:
+        opened_file.write(config_text + '\n')
+
+
+def enable_wireguard_service(interface):
+    subprocess.call(
+        [r'systemctl', r'start', r'wg-quick@' + interface + r'.service'])
+    subprocess.call(
+        [r'systemctl', r'enable', r'wg-quick@' + interface + r'.service'])
