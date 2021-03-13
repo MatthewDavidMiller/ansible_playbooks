861192ca9a0b52662ea2ce59ff3f152b410069cf
Matthew David Miller <MatthewMiller@mdmiller.dev>
2021-03-12 19:15:51 -0500

    Fixed syntax issues

--- linux_scripts/vpn_server_configure.py
+++ linux_scripts/vpn_server_configure.py
@@ -59,7 +59,7 @@ wireguard_public_dns_ip_address = env.wireguard_public_dns_ip_address
 user_name = env.user_name
 device_hostname = env.device_hostname
 packages = env.packages
-functions.get_interface_name
+functions.get_interface_name()
 interface = functions.interface
 
 # Option Menu
@@ -77,18 +77,18 @@ while True:
 
     selection = input(r'Select an option: ')
     if selection == '1':
-        functions.lock_root(user_name)
+        functions.lock_root()
         configure_network.configure_network(
             ip_address, network_address, subnet_mask, gateway_address, dns_address, interface, ipv6_link_local_address)
-        functions.install_packages
+        functions.install_packages(packages)
         vpn_server_scripts.install_vpn_server_packages()
         print(r'Reboot the OS before configuring wireguard: ')
         input()
     elif selection == '2':
-        configure_ssh.configure_ssh
+        configure_ssh.configure_ssh()
         generate_ssh_key.generate_ssh_key(
             user_name, r'y', r'n', r'n', key_name)
-        functions.setup_config_backups
+        functions.setup_config_backups()
         functions.configure_ddclient(wireguard_public_dns_ip_address)
         vpn_server_scripts.setup_basic_wireguard_interface(
             wireguard_interface, wireguard_server_ip_address)
@@ -103,15 +103,14 @@ while True:
         vpn_server_scripts.wireguard_create_client_config(user_name, wireguard_client_key_name, wireguard_server_vpn_key_name,
                                                           wireguard_client_ip_address, wireguard_dns_server, wireguard_public_dns_ip_address, wireguard_server_listen_port)
         vpn_server_scripts.enable_wireguard_service(wireguard_interface)
-        functions.get_interface_name
-        iptables_base.iptables_setup_base
+        iptables_base.iptables_setup_base()
         iptables_rules.iptables_allow_ssh(network_prefix, interface)
-        iptables_base.iptables_set_defaults
+        iptables_base.iptables_set_defaults()
         iptables_rules.iptables_allow_vpn_port(
             interface, wireguard_server_listen_port)
         iptables_rules.iptables_allow_icmp(network_prefix, interface)
-        iptables_rules.iptables_allow_loopback
-        iptables_base.iptables_allow_forwarding
+        iptables_rules.iptables_allow_loopback()
+        iptables_base.iptables_allow_forwarding()
         apt_auto_updates.apt_configure_auto_update_reboot()
         log_rotate_configure(user_name)
 
