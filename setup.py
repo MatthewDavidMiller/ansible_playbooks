85a073e7b5a3d57d7ee59c1d377b3bcdea49459e
Matthew David Miller <MatthewMiller@mdmiller.dev>
2021-03-19 23:06:21 -0400

    Added more of the base configuration.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+# Credits
+# https://www.tutorialspoint.com/python/os_chmod.htm
+
 import subprocess
+import os
+import pwd
+import stat
 
 subprocess.call([r'apt-get', r'update', r'-y'])
 
@@ -8,3 +14,12 @@ subprocess.call([r'apt-get', r'install', r'-y', r'ansible'])
 
 subprocess.call([r'ansible-galaxy', r'collection',
                 r'install', r'community.general'])
+
+subprocess.call([r'ansible-galaxy', r'collection',
+                r'install', r'community.crypto'])
+
+os.mkdir(r'/ansible_configs')
+uid = pwd.getpwnam(r'root').pw_uid
+gid = pwd.getpwnam(r'root').pw_gid
+os.chown(r'/ansible_configs' + uid, gid)
+os.chmod(r'/ansible_configs', stat.S_IRWXU)
